6900f79ca2c458d39c92a4bc1311257da4698438
DejonGill <dejonjgill@gmail.com>
2017-03-10 08:46:33 -0800

    Saving simple data to firebase

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -17,6 +21,18 @@ class App extends Component {
         data: snap.val()
       });
     });
+  }
+
+  handleChange(e) {
+    const newData = e.target.value;
+    this.setState({
+      newData
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    database.ref().child('AMAZING-NEW-DATA').set(this.state.newData);
   }
 
   render() {
@@ -30,6 +46,10 @@ class App extends Component {
         <pre className="App--data">
           { data === null ? '' : JSON.stringify(data, null, 2) }
         </pre>
+        <form className="App--form" onSubmit={ this.handleSubmit }>
+          <input type="text" value={ this.state.newData } onChange={ this.handleChange } />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
